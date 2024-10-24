dfaf7250c7680f15314fb153b853c36892e43da9
kohicha <yensyd.francisco@gmail.com>
2024-10-24 20:54:36 +0800

    first commit

--- Java Projects/FranciscoYensyd_BSCS1A/FranciscoYensyd_Activity1.java
+++
@@ -1,6 +0,0 @@
-public class FranciscoYensyd_Activity1{
-  public static void main (String[] args){
-    System.out.println("Hello");
-    System.out.println("Yensyd Symond Olayon Francisco");
-  }
-}
